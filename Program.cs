e389ad07c38e219c8ef69c01626f8ea9cdaf5d75
Dmitry Popov <dpopov0212@gmail.com>
2022-03-01 23:59:28 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeTaskLesson_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите свое имя");
+            string name = Console.ReadLine();
+
+            //Console.WriteLine("[" + DateTime.Now + "] " + "Hello, " + name + "!");
+            Console.WriteLine($"Привет, {name}, сегодня [{DateTime.Now}].");
+            Console.ReadKey();
+
+        }
+    }
+}
