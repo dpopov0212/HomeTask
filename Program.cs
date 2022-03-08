1bf65416b34602c5248ca4dface968463bac8ac6
Dmitry Popov <dpopov0212@gmail.com>
2022-03-08 14:13:37 +0300

    Изменение: отображение среднесуточной температуры

--- Program.cs
+++ Program.cs
@@ -4,19 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HomeTaskLesson_1
+namespace HomeTaskLesson_2
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args)//отображение среднесуточной температуры
         {
-            Console.WriteLine("Введите свое имя");
-            string name = Console.ReadLine();
+            Console.WriteLine("Введите минимальную температуру за сутки");
+            double mintemp = Convert.ToDouble(Console.ReadLine());
 
-            //Console.WriteLine("[" + DateTime.Now + "] " + "Hello, " + name + "!");
-            Console.WriteLine($"Привет, {name}, сегодня [{DateTime.Now}].");
+            Console.WriteLine("Введите максимальную температуру за сутки");
+            double maxtemp = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"Среднесуточная температура за сутки составляет: {(mintemp + maxtemp) / 2 }");
             Console.ReadKey();
-
         }
     }
 }
