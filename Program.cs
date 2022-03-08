7410c01f39aa09b4f89a618c684fb0265ba98091
Dmitry Popov <dpopov0212@gmail.com>
2022-03-08 17:02:22 +0300

    Выполнил ДЗ 2 по уроку 2

--- Program.cs
+++ Program.cs
@@ -4,20 +4,61 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HomeTaskLesson_2
+namespace HomeTaskLesson2
 {
     class Program
     {
-        static void Main(string[] args)//отображение среднесуточной температуры
+        static void Main(string[] args)//отображение названия месяца по порядковому номеру
         {
-            Console.WriteLine("Введите минимальную температуру за сутки");
-            double mintemp = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите порядковый номер текущего месяца");
+            int monthOfYear = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Введите максимальную температуру за сутки");
-            double maxtemp = Convert.ToDouble(Console.ReadLine());
+            string monthTitle = "";
+            switch (monthOfYear - 1)
+            {
+                case 0:
+                    monthTitle = "Январь";
+                    break;
+                case 1:
+                    monthTitle = "Февраль";
+                    break;
+                case 2:
+                    monthTitle = "Март";
+                    break;
+                case 3:
+                    monthTitle = "Апрель";
+                    break;
+                case 4:
+                    monthTitle = "Май";
+                    break;
+                case 5:
+                    monthTitle = "Июнь";
+                    break;
+                case 6:
+                    monthTitle = "Июль";
+                    break;
+                case 7:
+                    monthTitle = "Август";
+                    break;
+                case 8:
+                    monthTitle = "Сентябрь";
+                    break;
+                case 9:
+                    monthTitle = "Октябрь";
+                    break;
+                case 10:
+                    monthTitle = "Ноябрь";
+                    break;
+                case 11:
+                    monthTitle = "Декабрь";
+                    break;
+                default:
+                    Console.WriteLine("Укажите значение от 1 до 12");
+                    break;
+            }
 
-            Console.WriteLine($"Среднесуточная температура за сутки составляет: {(mintemp + maxtemp) / 2 }");
-            Console.ReadKey();
+            Console.WriteLine(monthTitle);
+
         }
     }
 }
