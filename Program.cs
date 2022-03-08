c0a65b91e0b6f69e7093153fbf39eb36267da852
Dmitry Popov <dpopov0212@gmail.com>
2022-03-08 17:36:05 +0300

    Выполнил ДЗ 4  урок 2

--- Program.cs
+++ Program.cs
@@ -8,21 +8,34 @@ namespace HomeTaskLesson2
 {
     class Program
     {
-        static void Main(string[] args)//определение четного/нечетного числа
+        static void Main(string[] args)//отображение чека
         {
-            Console.WriteLine("Введите число");
-            int number = Convert.ToInt32(Console.ReadLine());
-            double ostatok = number % 2;//вычисление остатка от деления числа на 2
+            string nameShop = "ЕВРОТОРГ";
+            string adress = "г.Гродно ул.Горького 55";
+            int kodeUNP = 500838837;
+            double sum = 0;
 
-            if (ostatok == 0)
-            {
-                Console.WriteLine($"Выбранное Вами число четное");
-            }
+            string[] nameProd = { "Сыр творожный САВУШКИН", "Зефир ПЕРВЫЙ БОБРУЙСКИЙ", "Сметана БРЕСТ ЛИТОВСКАЯ",
+                "Чай Greenfield", "Молоко МОЛОЧНЫЙ МИР", "Пакет-майка" };
+            double[] price = { 3.65, 2.69, 2.16, 4.79, 1.29, 0.30 };
 
-            else
-            {
-                Console.WriteLine($"Выбранное Вами число нечетное");
-            }
+            Console.WriteLine($"Магазин {nameShop}"); ; ;
+            Console.WriteLine(adress);
+            Console.WriteLine($"Код УНП {kodeUNP}");
+            Console.WriteLine($"Код УНП {kodeUNP}\n");
+
+            for (int i = 0; i < nameProd.Length; i++)
+                Console.WriteLine("{0, 10} {1,5:N1}", nameProd[i], price[i]);
+
+            for (int i = 0; i < price.Length; i++)
+                sum = sum + price[i];
+            Console.WriteLine($"Сумма покупок  {Math.Round(sum, 2)}");
+            Console.WriteLine($"Итого к оплате  {Math.Round(sum, 2)}");
+
+            Console.WriteLine($"Оплачено (Банк. карта) {Math.Round(sum, 2)}\n");
+            Console.WriteLine($"СПАСИБО ЗА ПОКУПКУ");
+            Console.WriteLine(DateTime.Now);
+
             Console.ReadKey();
         }
     }
