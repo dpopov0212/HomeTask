aacf338086bcbb55dc75525bcde2b1c7eb1dd6c2
Dmitry Popov <dpopov0212@gmail.com>
2022-03-10 00:19:02 +0300

    create new program L2T3

---
+++ Lesson2-Task3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Lesson2_Task3
+{
+    class Program
+    {
+        static void Main(string[] args)//определение четного/нечетного числа
+        {
+            Console.WriteLine("Введите число");
+            int number = Convert.ToInt32(Console.ReadLine());
+            double ostatok = number % 2;//вычисление остатка от деления числа на 2
+
+            if (ostatok == 0)
+            {
+                Console.WriteLine($"Выбранное Вами число четное");
+            }
+
+            else
+            {
+                Console.WriteLine($"Выбранное Вами число нечетное");
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
