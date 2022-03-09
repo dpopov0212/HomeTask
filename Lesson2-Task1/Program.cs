35dca792bfee988918219c3f8cfcc809f9a893d9
Dmitry Popov <dpopov0212@gmail.com>
2022-03-09 23:15:30 +0300

    did lesson2-task1 /отображение среднесуточной температуры

--- Lesson2-Task1/Program.cs
+++ Lesson2-Task1/Program.cs
@@ -4,9 +4,16 @@ namespace Lesson2_Task1
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args)//отображение среднесуточной температуры
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Введите минимальную температуру за сутки");
+            double mintemp = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите максимальную температуру за сутки");
+            double maxtemp = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"Среднесуточная температура за сутки составляет: {(mintemp + maxtemp) / 2 }");
+            Console.ReadKey();
         }
     }
 }
