80184086eea26a085566bb4daec7359fa1bc4f49
Dmitry Popov <dpopov0212@gmail.com>
2022-03-09 23:47:14 +0300

    create new project L2T1

--- Lesson2-Task1/Program.cs
+++ Lesson2-Task1/Program.cs
@@ -4,16 +4,9 @@ namespace Lesson2_Task1
 {
     class Program
     {
-        static void Main(string[] args)//отображение среднесуточной температуры
+        static void Main(string[] args)
         {
-            Console.WriteLine("Введите минимальную температуру за сутки");
-            double mintemp = Convert.ToDouble(Console.ReadLine());
-
-            Console.WriteLine("Введите максимальную температуру за сутки");
-            double maxtemp = Convert.ToDouble(Console.ReadLine());
-
-            Console.WriteLine($"Среднесуточная температура за сутки составляет: {(mintemp + maxtemp) / 2 }");
-            Console.ReadKey();
+            Console.WriteLine("Hello World!");
         }
     }
 }
