b9e2f8c39169bbbd792896881763c8d2ed8e7aaf
Dmitry Popov <dpopov0212@gmail.com>
2022-03-10 19:26:08 +0300

    create program L3T1

---
+++ Lesson3-Task1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Lesson3_Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] myarray = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+
+            for (int i = 0; i < myarray.GetLength(0); i++)
+            {
+                for (int j = 0; j < myarray.GetLength(1); j++)
+                {
+                    Console.Write(myarray[i, j] + "  ");
+                }
+                Console.WriteLine();
+            }
+
+                Console.Write("\n Диагональ: \n");
+                for (int i = 0; i < myarray.GetLength(0); i++) //отображение диагонали массива
+                {
+                    Console.Write(myarray[i, i] + "  ");
+                }
+            
+        }
+    }
+}
