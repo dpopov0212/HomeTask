b34327f892a7a153fb451d6ef0b37734b60af69b
Dmitry Popov <dpopov0212@gmail.com>
2022-04-30 08:10:32 +0300

    1

---
+++ Lesson7/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число!");
+            int first = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число!");
+            int second = Convert.ToInt32(Console.ReadLine());
+
+            if (first > second)
+            {
+                Console.WriteLine($"Число {first} > числа {second}");
+            }
+
+            else if (first < second)
+            {
+                Console.WriteLine($"Число {first} < числа {second}");
+
+            }
+
+            else
+            {
+                Console.WriteLine($"Число {first} = числу {second}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
